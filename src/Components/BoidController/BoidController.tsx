e94b6cd8471258f658af5af8fafb6857ea8fc19a
Andrej Nenadić <andrejnana445@gmail.com>
2024-09-26 12:39:52 +0200

    Make boids teleport to the opposite edge when leaving the screen

--- src/Components/BoidController/BoidController.tsx
+++ src/Components/BoidController/BoidController.tsx
@@ -1,6 +1,6 @@
 import "./BoidController.scss";
-import { Canvas, useFrame } from "@react-three/fiber";
-import { useEffect, useMemo, useState } from "react";
+import { Canvas, useFrame, useThree } from "@react-three/fiber";
+import { useEffect, useState } from "react";
 import Boid from "../../ClassDefinitions/Boid";
 import BoidRenderer from "../Boid/BoidRenderer";
 
@@ -21,14 +21,28 @@ export default function BoidController({ initialBoids }: BoidControllerProps) {
 function InnerBoidController({ initialBoids }: BoidControllerProps) {
   const [boids, setBoids] = useState<Boid[]>([]);
   useEffect(() => void setBoids(initialBoids), [initialBoids]);
+  const {
+    viewport: { width: viewportWidth, height: viewportHeight },
+  } = useThree();
 
   useFrame(() => {
     setBoids((boids) => {
       boids.forEach((boid) => {
         const velocity = boid.forward.copy();
-        velocity.truncate(0.01);
+        velocity.truncate(0.05); //Limit the maximum velocity //TODO: pull this out to a constant or a prop
 
         boid.position.add(velocity);
+
+        if (boid.position.x < -viewportWidth / 2)
+          boid.position.x = viewportWidth / 2;
+        else if (boid.position.x > viewportWidth / 2)
+          boid.position.x = -viewportWidth / 2;
+
+        if (boid.position.y < -viewportHeight / 2)
+          boid.position.y = viewportHeight / 2;
+        else if (boid.position.y > viewportHeight / 2)
+          boid.position.y = -viewportHeight / 2;
+
         console.log(boid.position);
       });
 
